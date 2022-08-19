59150a516279db95ca94fee51778a3aceb312c1c
Maryna-Pokhyliuk19 <99185203+Maryna-Pokhyliuk19@users.noreply.github.com>
2022-08-19 12:33:54 +0000

    Deploying to gh-pages from @ Maryna-Pokhyliuk19/goit-js-hw-08@8ae047525f507d27fd870d25f589cc2b367c253b 🚀

--- 03-feedback.a8c6315b.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector("feedback-form"),textarea:document.querySelector("feedback-form textarea")};e.form.addEventListener("submit",(function(e){e.preventDefauil(),e.currentTarget.reset(),localStorage.removeItem("feedback-form-state")})),e.textarea.addEventListener("input",(function(e){var t=e.target.value;localStorage.setItem("feedback-form-state",t)}))}();
-//# sourceMappingURL=03-feedback.a8c6315b.js.map
